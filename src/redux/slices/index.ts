09b4febfaebad0a78ac595c570c0b9667e3e7f3d
“ireshan” <ireshandj2@gmail.com>
2024-10-28 02:53:41 +0800

    my hrm: setup redux store with API and Slice

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -1,7 +1,12 @@
-import { combineSlices } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
 import { counterSlice } from "./counterSlice";
+import { fileManagementApi } from "../api/fileManagmentAPI";
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice);
-
+const rootReducer = combineReducers({
+    // Non-API slices
+    [counterSlice.reducerPath]: counterSlice.reducer,
+    // API slices (RTK Query reducers)
+    [fileManagementApi.reducerPath]: fileManagementApi.reducer,
+});
 export default rootReducer;
