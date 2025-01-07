fba2cda2213b96bb151bbbc486d66a66076d8b8a
“ireshan” <ireshandj2@gmail.com>
2025-01-07 19:35:03 +0800

    my hrm: add notifications and theam change buttons into header

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import { Separator } from '@/components/ui/separator';
 import BreadcrumbComponent from '@/components/molecules/Breadcrumb';
 import { AppSidebar } from './app-sidebar';
+import Header from './header';
 
 export const metadata: Metadata = {
   title: 'Next Shadcn Dashboard Starter',
@@ -21,13 +22,7 @@ export default function DashboardLayout({
       <AppSidebar />
       <SidebarInset>
         <main className="w-full flex-1 overflow-hidden">
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <BreadcrumbComponent items={[{ label: 'home', href: '/dashboard/employees'  },{ label: 'Dashboard',  }]} />
-            </div>
-          </header>
+          <Header />
           {children}
         </main>
       </SidebarInset>
