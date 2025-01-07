c0599f051605725b7ed9a164f8b2853908dc8af1
“ireshan” <ireshandj2@gmail.com>
2025-01-07 19:36:27 +0800

    my hrm: create intial app sclice to hold application common data

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { counterSlice } from "./counterSlice";
 import { fileManagementApi } from "../api/fileManagmentAPI";
 import { authManagementApi } from "../api/authManagementAPI";
+import { appSlice } from "./appSlice";
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
 const rootReducer = combineReducers({
     // Non-API slices
     [counterSlice.reducerPath]: counterSlice.reducer,
+    [appSlice.reducerPath]: appSlice.reducer,
     // API slices (RTK Query reducers)
     [fileManagementApi.reducerPath]: fileManagementApi.reducer,
     [authManagementApi.reducerPath]: authManagementApi.reducer,
