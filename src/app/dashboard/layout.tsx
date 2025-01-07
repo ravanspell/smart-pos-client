6ea599254ee3124053b181fed9101bb643f136c4
“ireshan” <ireshandj2@gmail.com>
2025-01-07 12:59:40 +0800

    my hrm: dashboard side bar layout issue fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,10 @@
-import Header from './header';
-import Sidebar from './sideBar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+// import Header from './header';
+// import Sidebar from './sideBar';
 import type { Metadata } from 'next';
+import { Separator } from '@/components/ui/separator';
+import BreadcrumbComponent from '@/components/molecules/Breadcrumb';
+import { AppSidebar } from './app-sidebar';
 
 export const metadata: Metadata = {
   title: 'Next Shadcn Dashboard Starter',
@@ -13,12 +17,20 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="w-full flex-1 overflow-hidden">
-        <Header />
-        {children}
-      </main>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <main className="w-full flex-1 overflow-hidden">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <BreadcrumbComponent items={[{ label: 'home', href: '/dashboard/employees'  },{ label: 'Dashboard',  }]} />
+            </div>
+          </header>
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
