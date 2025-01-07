08f7a1c88e044a9a0b6fa8c2855dfeac461be8d9
“ireshan” <ireshandj2@gmail.com>
2025-01-07 19:35:46 +0800

    my hrm: rename create app slice into create async slice

--- src/redux/slices/createAsyncSlice.ts
+++ src/redux/slices/createAsyncSlice.ts
@@ -1,6 +1,6 @@
 import { asyncThunkCreator, buildCreateSlice } from "@reduxjs/toolkit";
 
 // `buildCreateSlice` allows us to create a slice with async thunks.
-export const createAppSlice = buildCreateSlice({
+export const createAsyncSlice = buildCreateSlice({
   creators: { asyncThunk: asyncThunkCreator },
 });
