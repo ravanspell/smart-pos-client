80816f82128fed530a6a8540db14d8387033ecec
“ireshan” <ireshandj2@gmail.com>
2025-01-11 11:22:44 +0800

    my hrm: CORS settings to handle cookies

--- src/redux/api/api.tsx
+++ src/redux/api/api.tsx
@@ -1,6 +1,10 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const baseQuery = fetchBaseQuery({ 
+export const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:3001/api/',
     credentials: 'include',
+    headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Credentials': 'true'
+    }
 });
