5a0fb86dda40c9a91bb48c66c043687b864acdf2
“ireshan” <ireshandj2@gmail.com>
2025-01-11 10:50:08 +0800

    my hrm: routes const and enable cookies for APIs

--- src/redux/api/api.tsx
+++ src/redux/api/api.tsx
@@ -1,5 +1,6 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseQuery = fetchBaseQuery({ 
-    baseUrl: 'http://localhost:3001/api/'
+    baseUrl: 'http://localhost:3001/api/',
+    credentials: 'include',
 });
