59d0e47373e1d9dd06d449e76110ab62d57e2b83
“ireshan” <ireshandj2@gmail.com>
2025-01-14 00:20:38 +0800

    my hrm: general setting page as default settings page

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,4 @@
 export const FILE_MANAGEMENT_ROUTE = '/dashboard/file-management';
 export const DASHBOARD_ROUTE = '/dashboard';
 export const EMPLOYEES_ROUTE = '/dashboard/employees';
-export const SETTINGS_ROUTE = '/dashboard/settings';
+export const DEFAULT_SETTINGS_ROUTE = '/dashboard/settings/general';
