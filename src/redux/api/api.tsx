76e60f2c95e6b4cfd778c444f94ddb15c3a34764
“ireshan” <ireshandj2@gmail.com>
2025-01-14 00:17:23 +0800

    my hrm: add API retry

--- src/redux/api/api.tsx
+++ src/redux/api/api.tsx
@@ -1,10 +1,16 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
 
-export const baseQuery = fetchBaseQuery({
+/**
+ * Global setup of the redux toolkit RTK API
+ * Setup provides retries with a basic exponential backoff.
+ */
+export const baseQuery = retry(fetchBaseQuery({
     baseUrl: 'http://localhost:3001/api/',
     credentials: 'include',
     headers: {
         'Access-Control-Allow-Origin': 'http://localhost:3000',
         'Access-Control-Allow-Credentials': 'true'
-    }
+    },
+}), {
+    maxRetries: 5, // default 5
 });
