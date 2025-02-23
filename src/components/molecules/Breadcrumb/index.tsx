9cc91c249097f90a822f3fab7aa114a5c125c49c
“ireshan” <ireshandj2@gmail.com>
2025-02-23 22:29:45 +0800

    my hrm: breadcrumb component update with new york styles

--- src/components/molecules/Breadcrumb/index.tsx
+++ src/components/molecules/Breadcrumb/index.tsx
@@ -5,7 +5,7 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+} from "@/components/atoms/Breadcrumb"
 import {
   DropdownMenu,
   DropdownMenuContent,
