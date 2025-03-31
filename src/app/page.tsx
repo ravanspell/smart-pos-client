321642510ccd1c97037675485bab75a83ffd4c9b
“ireshan” <ireshandj2@gmail.com>
2025-03-31 12:21:52 +0800

    my hrm: setup redirect to login page - temporary provitioned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { LOGIN_ROUTE } from "@/constants/routes";
 
 export default function IndexPage() {
-  return <div>Sample page</div>;
+  // temp redirect to login page
+  redirect(LOGIN_ROUTE);
 }
 
 export const metadata: Metadata = {
-  title: "Redux Toolkit",
+  title: "My HRM",
 };
